46e52837aea51ed29d02bb6bfa315156883d1ed4
devxml <devverma2031@gmail.com>
2025-08-26 11:42:17 +0530

    Initialize TypeScript project

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,7 @@ import Navbar from "./components/Navbar";
 import Header from "./components/Header";
 import Contactform from "./components/Contactform";
 import Skills from "./components/Skills";
-import Education from "./components/Education";
+import Education from "./components/Education"; 
 import Projects from "./components/Projects";
 import Footer from "./components/Footer";
 import Experience from "./components/Experience";
