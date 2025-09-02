b3b109779090f2c53ab88a9f11f3591fad4e6f40
devxml <devverma2031@gmail.com>
2025-09-02 13:00:31 +0530

    Configure TypeScript and ESLint

--- app/Theme.tsx
+++ app/Theme.tsx
@@ -4,8 +4,9 @@ import { createContext } from "react";
 const Contextfirst = createContext<{
   image:boolean,
   setImage:React.Dispatch<React.SetStateAction<boolean>>,
-  mode: string;
+  mode: string; 
   Setmode: React.Dispatch<React.SetStateAction<string>>;
 } | null>(null);
-
-export default Contextfirst;
+ 
+export default Contextfirst; 
+  
